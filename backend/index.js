cd64d0a1b2c05145de0c1a9142891fe6175f1452
BICHRI <emanbichri@gmail.com>
2025-02-07 17:30:12 +0100

    adjust frontend link in index.js

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,11 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+
 
 // Middleware
 app.use(cors({
-    origin: FRONTEND_URL,
+    origin: 'https://tetris-satisfaction-clients.vercel.app',
     methods: "GET,POST,PUT,DELETE",
     credentials: true, 
   }));
