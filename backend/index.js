3ea7d25732613d9a81cf075ed633e1d3560429f7
BICHRI <emanbichri@gmail.com>
2025-02-07 14:15:51 +0100

    new database azure config

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,28 @@ const mariadb = require('mariadb');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Database configuration
+require('dotenv').config(); // Only needed for local development
+
 const pool = mariadb.createPool({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123',
-    database: 'satisfaction_db',
-    connectionLimit: 5,
-    bigIntAsNumber: true  // Convert BigInt to Number
+    host: process.env.DB_HOST, 
+    port: process.env.DB_PORT, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME, 
+    connectionLimit: 5, 
+    bigIntAsNumber: true
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 // Création de la table low_satisfaction_responses (inchangé)
